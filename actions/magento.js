8ca8a54fa7436df6ad666b38a0400bb4ddd41278
Lincoln Lo <Lolincoln23@gmail.com>
2018-07-24 13:30:49 -0400

    quick fix

--- actions/magento.js
+++ actions/magento.js
@@ -5,7 +5,6 @@ let magento = require('@datafire/magento').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
         let result = await magento.customerCustomerMetadataV1GetCustomAttributesMetadataGet({}, context);
-        return result;
     return result;
   },
 });
