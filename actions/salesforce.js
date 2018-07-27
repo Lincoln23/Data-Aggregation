adb83451c41630885d1f6da41a5837e0e3b6577b
Lincoln Lo <Lolincoln23@gmail.com>
2018-07-27 12:24:44 -0400

    Correcting Async SQL code

--- actions/salesforce.js
+++ actions/salesforce.js
@@ -1,92 +1,97 @@
 "use strict";
 const datafire = require('datafire');
-const db = require('./setup');
+const db = require('./setup.js');
 let fs = require('fs');
-
 
 let salesforce = require('@datafire/salesforce').actions;
 module.exports = new datafire.Action({
-    handler: async (input, context) => {
-        let endResult;
+    //TODO sync time first
+    handler: (input, context) => {
+
+        let database = new db(config);
         let newDataContact = 0; // set to true only if there is new data and it will update the last synced time
         let newDataOpportunity = 0; // set to true only if there is new data and it will update the last synced time
         console.log('in salesforce');
-        console.log('in salesforce');
-        let dateContacts;
-        let dateOpportunity;
+        let contactsSyncTime;
+        let opportunitySyncTime;
+
         if (!fs.existsSync('./SalesForce.txt')) {
-            dateOpportunity = dateContacts = "1970-01-15T00:00:00.000Z";
-            let sqlSyncContact = 'INSERT INTO SyncTime (Name, Time) VALUES (?,?)';
-            let syncContactValues = ["SalesForceContact", newDataContact];
-            db.query(sqlSyncContact, syncContactValues, (err) => {
-                if (err) throw err;
-                console.log("success inserting to SyncTime for SalesForceContact");
-            });
-            let sqlSyncOpportunity = 'INSERT INTO SyncTime (Name, Time) VALUES (?,?)';
-            let opportunityValues = ["SalesForceOpportunity", dateOpportunity];
-            db.query(sqlSyncOpportunity, opportunityValues, (err) => {
-                if (err) throw err;
-                console.log("success inserting to SyncTime for SalesForceOpportunity");
-            });
+
             fs.writeFile("SalesForce.txt", "SalesForce Synced for the first time", function (err) {
-                if (err) {
-                    console.log(err);
-                }
+                if (err) console.log(err);
                 console.log("The file was saved!");
+            });
+            opportunitySyncTime = contactsSyncTime = "1970-01-15T00:00:00.000Z";
+            let sqlSyncContact = 'INSERT INTO SyncTime (Name, Time) VALUES (?,?)';
+            let syncContactValues = ["SalesForceContact", contactsSyncTime];
+            database.query(sqlSyncContact, syncContactValues).then(() => {
+                console.log("success inserting to SyncTime for SalesForceContact");
+            }).catch((err) => {
+                console.log("Error inserting to SyncTime for SalesForceContact, Message: " + err);
+            });
+
+            let sqlSyncOpportunity = 'INSERT INTO SyncTime (Name, Time) VALUES (?,?)';
+            let opportunityValues = ["SalesForceOpportunity", opportunitySyncTime];
+            database.query(sqlSyncOpportunity, opportunityValues).then(() => {
+                console.log("success inserting to SyncTime for SalesForceOpportunity");
+            }).catch((err) => {
+                console.log("Error inserting to SyncTime for SalesForceOpportunity, Message: " + err);
             });
         }
 
-        await new Promise((resolve, reject) => {
-            db.query('SELECT TIME AS Current FROM SyncTime WHERE NAME = "SalesForceContact"', (err, result) => {
-                if (err) throw err;
-                dateContacts = result[0].Current;
-            });
-            resolve();
+
+        database.query('SELECT TIME AS Current FROM SyncTime WHERE NAME = "SalesForceContact"').then(result => {
+            console.log(result);
+            contactsSyncTime = result[0].Current;
+            console.log("Sync Time for contactsSyncTime:" + contactsSyncTime);
+        }).catch((err) => {
+            console.log("Error selecting from SyncTime for SalesForceContact, Message: " + err);
         });
-        await new Promise((resolve, reject) => {
-            db.query('SELECT TIME AS Current FROM SyncTime WHERE NAME = "SalesForceOpportunity" ', (err, result) => {
-                if (err) throw err;
-                dateOpportunity = result[0].Current
-            });
-            resolve();
+
+        database.query('SELECT TIME AS Current FROM SyncTime WHERE NAME = "SalesForceOpportunity" ').then(result => {
+            console.log(result);
+            opportunitySyncTime = result[0].Current;
+            console.log("Sync Time for opportunitySyncTime:" + opportunitySyncTime);
+        }).catch(err => {
+            console.log("Error selecting from SyncTime for SalesForceOpportunity, Message: " + err);
         });
-        console.log(dateContacts);
-        console.log(dateOpportunity);
+
 
         //queries for contacts
         let queryContacts = new Promise((resolve, reject) => {
-            let result = salesforce.version.query.get({
-                version: "v24.0",
-                q: "SELECT Id, Name, email, phone, Account.Name, Account.Id, contact.owner.Alias FROM Contact WHERE (LastModifiedDate > " + dateContacts +
-                    ") ORDER BY Name ASC ",
-            }, context);
-            let temp2 = "SELECT Id, Name, email, phone, Account.Name, Account.Id, contact.owner.Alias FROM Contact WHERE (LastModifiedDate > " + dateContacts +
-                ") ORDER BY Name ASC ";
-            console.log(temp2);
-            resolve(result);
-        });
-        //queries for Opporunites
-        let queryForOpportunities = new Promise((resolve, reject) => {
-            let result = salesforce.version.query.get({
-                version: "v24.0",
-                q: "SELECT id, opportunity.name, Account.Name, Accountid, Amount, Createddate, closedate, stageName, expectedRevenue From Opportunity WHERE (LastModifiedDate > " + dateOpportunity + " ) ORDER BY Name ASC",
-            }, context);
-            let temp = "SELECT id, opportunity.name, Account.Name, Accountid, Amount, Createddate, closedate, stageName, expectedRevenue From Opportunity WHERE (LastModifiedDate > " + dateOpportunity + " ) ORDER BY Name ASC";
-            console.log(temp);
-            resolve(result);
-        });
-        try {
-            endResult = await Promise.all([queryContacts, queryForOpportunities]);
-        } catch (e) {
-            return e;
-        }
-
-
-        let contacts = endResult[0].records; //contacts object
-        let opportunity = endResult[1].records; // opportunity object
-        contacts.forEach(element => {
-            newDataContact = 1;
             try {
+                let resultContact = salesforce.version.query.get({
+                    version: "v24.0",
+                    q: "SELECT Id, Name, email, phone, Account.Name, Account.Id, contact.owner.Alias FROM Contact WHERE (LastModifiedDate > " + contactsSyncTime +
+                        ") ORDER BY Name ASC ",
+                }, context);
+                resolve(resultContact)
+            } catch (e) {
+                reject(e);
+            }
+        }).catch(e => {
+            console.log("Error in queryContacts. Message: " + e);
+        });
+        //queries for Opportunities
+        let queryOpportunity = new Promise((resolve, reject) => {
+            try {
+                let resultOpportunities = salesforce.version.query.get({
+                    version: "v24.0",
+                    q: "SELECT id, opportunity.name, Account.Name, Accountid, Amount, Createddate, closedate, stageName, expectedRevenue From Opportunity WHERE (LastModifiedDate > " + opportunitySyncTime + " ) ORDER BY Name ASC",
+                }, context);
+                resolve(resultOpportunities);
+            } catch (e) {
+                reject(e);
+            }
+        }).catch(e => {
+            console.log("Error in queryOpportunity. Message: " + e);
+        });
+
+        Promise.all([queryContacts, queryOpportunity]).then((value) => {
+            let contacts = value[0].records; //contacts object
+            let opportunity = value[1].records; // opportunity object
+            contacts.forEach(element => {
+                newDataContact = 1;
                 //For objects that are node, need to set the properties inside to be null as well
                 if (element.Account == null) {
                     element.Account = {
@@ -100,48 +105,48 @@ module.exports = new datafire.Action({
                 }
                 let sqlContact = 'INSERT INTO SalesForceContact (ContactId, Name, Email, Phone,AccountName, AccountID,Alias) VALUES (?,?,?,?,?,?,?)';
                 let contactValues = [element.Id, element.Name, element.Email, element.Phone, element.Account.Name, element.Account.Id, element.Owner.Alias];
-                db.query(sqlContact, contactValues, (err) => {
-                    if (err) throw err;
+                try {
+                    database.query(sqlContact, contactValues, (err) => {
+                        if (err) throw err;
+                        console.log("success inserting to SalesForceContact");
+                    });
+                } catch (err) {
+                    console.log('Error Occurred in Contacts.foreach loop, Message: ' + err);
+                }
+            });
 
-                    console.log("success inserting to SalesForceContact");
-                });
-            } catch (e) {
-                console.log('Error Occurred in Contacts.foreach loop, Message: ' + e);
-            }
-        });
-
-        opportunity.forEach(value => {
-            newDataOpportunity = 1;
-            try {
+            opportunity.forEach(value => {
+                newDataOpportunity = 1;
                 let sqlOpportunity = 'INSERT INTO SalesForceOpportunity (OpportunityId, Name, AccountName, AccountID,Amount, CreatedDate,CloseDate,StageName,ExpectedRevenue) VALUES (?,?,?,?,?,?,?,?,?)';
                 let opportunityValues = [value.Id, value.Name, value.Account.Name, value.AccountId, value.Amount, value.CreatedDate, value.CloseDate, value.StageName, value.ExpectedRevenue];
-                db.query(sqlOpportunity, opportunityValues, (err) => {
-                    if (err) throw err;
-                    console.log("success inserting to SalesForceOpportunity");
-                });
-            } catch (e) {
-                console.log('Error Occurred in opportunity.foreach loop, Message: ' + e);
-            }
+                try {
+                    database.query(sqlOpportunity, opportunityValues, (err) => {
+                        if (err) throw err;
+                        console.log("success inserting to SalesForceOpportunity");
+                    });
+                } catch (err) {
+                    console.log('Error Occurred in opportunity.foreach loop, Message: ' + err);
+                }
 
+            });
+            console.log(newDataOpportunity + " There is new Data to Opportunity");
+            console.log(newDataContact + " There is new Data to Contacts");
+            if (newDataContact) { //only update time if there is new data
+                contactsSyncTime = new Date().toISOString();
+                let sqlContact = 'Update SyncTime Set Time = ? WHERE Name = "SalesForceContact" ';
+                database.query(sqlContact, contactsSyncTime, (err) => {
+                    if (err) throw err;
+                    console.log("success updating SyncTime for SalesForceContact");
+                });
+            }
+            if (newDataOpportunity) {
+                opportunitySyncTime = new Date().toISOString();
+                let sqlOpportunity = 'Update SyncTime Set Time = ? WHERE Name = "SalesForceOpportunity" ';
+                database.query(sqlOpportunity, opportunitySyncTime, (err) => {
+                    if (err) throw err;
+                    console.log("success updating SyncTime for SalesForceOpportunity ");
+                });
+            }
         });
-        console.log(newDataOpportunity + " newDataOPP");
-        console.log(newDataContact + " newDataCOn");
-        if (newDataContact) { //only update time if there is new data
-            dateContacts = new Date().toISOString();
-            let sqlContact = 'Update SyncTime Set Time = ? WHERE Name = "SalesForceContact" ';
-            db.query(sqlContact, dateContacts, (err) => {
-                if (err) throw err;
-                console.log("success updating SyncTime for SalesForceContact");
-            });
-        }
-        if (newDataOpportunity) {
-            dateOpportunity = new Date().toISOString();
-            let sqlOpportunity = 'Update SyncTime Set Time = ? WHERE Name = "SalesForceOpportunity" ';
-            db.query(sqlOpportunity, dateOpportunity, (err) => {
-                if (err) throw err;
-                console.log("success updating SyncTime for SalesForceOpportunity ");
-            });
-        }
-        return endResult;
     },
 });
