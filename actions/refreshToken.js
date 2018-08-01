0c25120d1f8be7506889feeef116dd7a63b32d6d
Lincoln Lo <Lolincoln23@gmail.com>
2018-08-01 16:53:09 -0400

    refreshes access tokens that are about to expire in 15 minutes

---
+++ actions/refreshToken.js
@@ -0,0 +1,92 @@
+"use strict";
+let request = require("request");
+const datafire = require('datafire');
+let webUrl = require('../auth');
+const db = require('./setup.js');
+let config = require('./config.json');
+
+//varchar length was too short, didnt get all
+let database = new db(config);
+let refresh = (id, secret, refreshToken, integration) => {
+    let tokenUrl = webUrl[integration].refresh;
+    console.log(id);
+    console.log(secret);
+    console.log(refreshToken);
+    console.log(integration);
+    console.log(tokenUrl);
+    let options = {
+        method: 'POST',
+        url: tokenUrl,
+        headers:
+            {
+                'Cache-Control': 'no-cache',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        form:
+            {
+                refresh_token: refreshToken,
+                grant_type: 'refresh_token',
+                client_id: id,
+                client_secret: secret
+            }
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+        let jsonBody = JSON.parse(body);
+        console.log(jsonBody.access_token);
+        console.log(jsonBody.refresh_token);
+        console.log(jsonBody.expires_in);
+        if (jsonBody.error) {
+            console.log(jsonBody.error);
+        } else {
+            if (integration == 'gmail' || integration == 'google_sheets' || integration == 'google_calendar' || integration == 'google_analytics') {
+                jsonBody.refresh_token = refreshToken;
+            }
+            let date = null;
+            if (jsonBody.expires_in != undefined) {
+                date = expiryDate(jsonBody.expires_in);
+            }
+            let sql = 'UPDATE AccessKeys SET AccessToken = ?, RefreshToken = ? ,Expiry = ? ,ExpiryDate = ? WHERE Name = ?';
+            let values = [jsonBody.access_token, jsonBody.refresh_token, jsonBody.expires_in, date, integration];
+            database.query(sql, values).catch(err => {
+                console.log("Error updating refreshTokens in AccessKeys, Message: " + err);
+            });
+            console.log("success updating refreshTokens for AccessKeys");
+        }
+    });
+};
+
+let expiryDate = (seconds) => {
+    let date = new Date();
+    let hours = Math.floor(seconds / 3600);
+    if (hours < 24) {
+        date.setHours(date.getHours() + hours);
+    } else {
+        let days = Math.floor(hours / 24);
+        date.setDate(date.getDate() + days)
+    }
+    return date.toISOString();
+};
+
+module.exports = new datafire.Action({
+    handler: () => {
+        let integration = null;
+        let refreshToken = null;
+        let clientID = null;
+        let clientSecret = null;
+        database.query("SELECT Name, RefreshToken, ClientId, ClientSecret from AccessKeys WHERE (TIMESTAMPDIFF(MINUTE,NOW(),ExpiryDate)) <= 15").then(result => {
+            console.log(result);
+            result.forEach(value => {
+                integration = value.Name;
+                refreshToken = value.RefreshToken;
+                clientID = value.ClientId;
+                clientSecret = value.ClientSecret;
+                refresh(clientID, clientSecret, refreshToken, integration);
+            })
+        }).catch((err) => {
+            console.log("Error selecting refreshTimes from AccessKeys, Message: " + err);
+        });
+        return "refresh in progress";
+    },
+});
