6909a69fd9cffa260969e91b3cc57e388eb54fab
Lincoln Lo <Lolincoln23@gmail.com>
2018-08-31 11:20:45 -0400

    Added logging to notify when process is starting

--- startup.js
+++ startup.js
@@ -1,4 +1,6 @@
 let datafire = require('datafire');
+let logger = require('./actions/winston');
+logger.accessLog.info("Starting Data-Integration");
 let project = datafire.Project.fromDirectory(__dirname);
 project.startTasks();
 project.serve({port: 3000});
