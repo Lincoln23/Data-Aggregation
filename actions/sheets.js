13c8b2317e25e26f5acd5f7a9425c71e8fdefdbc
Lincoln Lo <Lolincoln23@gmail.com>
2018-08-31 11:21:15 -0400

    Fixed a bug with Database connection error

--- actions/sheets.js
+++ actions/sheets.js
@@ -29,6 +29,7 @@ module.exports = new datafire.Action({
             logger.accessLog.info("Getting credentials in google_sheets for " + input.accountName);
             await database.query("SELECT AccessToken,RefreshToken,ClientId,ClientSecret FROM AccessKeys WHERE IntegrationName = 'google_sheets' AND Active = 1 AND AccountName = ?", input.accountName).then(result => {
                 result = result[0];
+                console.log(result);
                 google_sheets = require('@datafire/google_sheets').create({
                     access_token: result.AccessToken,
                     refresh_token: result.RefreshToken,
@@ -38,46 +39,6 @@ module.exports = new datafire.Action({
             }).catch(e => {
                 logger.errorLog.error("Error selecting from credentials in google_sheets for " + input.accountName + " " + e);
             });
-            if (google_sheets == null) {
-                logger.errorLog.warn("Integration disabled or invalid accountName in google_sheets for " + input.accountName);
-                return {error: "Invalid AccountName or integration disabled"};
-            }
-            logger.accessLog.verbose("Syncing google_sheets for " + input.accountName);
-            let startRow = 1;
-            let endRow = 9999;
-            let startCol = 1;
-            let endCol = inputs.length;
-            return await datafire.flow(context)
-                .then(_ => google_sheets.spreadsheets.values.get({
-                    spreadsheetId: input.spreadSheetId,
-                    range: getColumnLetter(startCol) + startRow + ':' + getColumnLetter(endCol) + endRow,
-                    valueRenderOption: "UNFORMATTED_VALUE",
-                }, context))
-                .then(data => {
-                    let rows = (data.values || []).map((row, rowNum) => {
-                        let obj = {
-                            id: rowNum + 1
-                        };
-                        inputs.forEach((input, idx) => {
-                            obj[input.title] = row[idx]
-                        });
-                        return obj;
-                    });
-
-                    rows.forEach(async json => {
-                        let createTableIfNotExist = "CREATE TABLE IF NOT EXISTS GoogeSheetsContacts( id int auto_increment primary key, Name varchar(255) , Organization varchar(1024) , Phone varchar(1024) , Email varchar(1024) , Location varchar(1024) )";
-                        database.query(createTableIfNotExist).catch(err => {
-                            logger.errorLog.error("Error creating table GoogeSheetsContacts" + err);
-                        }).then(() => {
-                            let sql = 'INSERT INTO GoogeSheetsContacts (Name, Organization, Phone, Email, Location) VALUES (?,?,?,?,?)';
-                            let values = [json.name, json.organization, json.phone, json.Email, json.City];
-                            database.query(sql, values).catch(e => {
-                                logger.errorLog.error("Error inserting into GoogeSheetsContacts for " + input.accountName + " " + e);
-                            });
-                        })
-                    });
-                    return rows;
-                })
         } finally {
             try {
                 await database.close();
@@ -85,5 +46,55 @@ module.exports = new datafire.Action({
                 logger.errorLog.error("Error closing database in sheets.js " + e);
             }
         }
+
+        if (google_sheets == null) {
+            logger.errorLog.warn("Integration disabled or invalid accountName in google_sheets for " + input.accountName);
+            return {error: "Invalid AccountName or integration disabled"};
+        }
+        logger.accessLog.verbose("Syncing google_sheets for " + input.accountName);
+        let startRow = 1;
+        let endRow = 9999;
+        let startCol = 1;
+        let endCol = inputs.length;
+        let result = await datafire.flow(context)
+            .then(_ => google_sheets.spreadsheets.values.get({
+                spreadsheetId: input.spreadSheetId,
+                range: getColumnLetter(startCol) + startRow + ':' + getColumnLetter(endCol) + endRow,
+                valueRenderOption: "UNFORMATTED_VALUE",
+            }, context))
+            .then(async data => {
+                let rows = (data.values || []).map((row, rowNum) => {
+                    let obj = {
+                        id: rowNum + 1
+                    };
+                    inputs.forEach((input, idx) => {
+                        obj[input.title] = row[idx]
+                    });
+                    console.log(obj);
+                    return obj;
+                });
+                return rows;
+            });
+        let db = new setup.database(config);
+        try {
+            let createTableIfNotExist = "CREATE TABLE IF NOT EXISTS GoogeSheetsContacts( id int auto_increment primary key, Name varchar(255) , Organization varchar(1024) , Phone varchar(1024) , Email varchar(1024) , Location varchar(1024) )";
+            db.query(createTableIfNotExist).catch(err => {
+                logger.errorLog.error("Error creating table GoogeSheetsContacts" + err);
+            });
+            result.forEach(async json => {
+                let sql = 'INSERT INTO GoogeSheetsContacts (Name, Organization, Phone, Email, Location) VALUES (?,?,?,?,?)';
+                let values = [json.name, json.organization, json.phone, json.Email, json.City];
+                db.query(sql, values).catch(e => {
+                    logger.errorLog.error("Error inserting into GoogeSheetsContacts for " + input.accountName + " " + e);
+                });
+            });
+        } finally {
+            try {
+                await db.close()
+            } catch (e) {
+                logger.errorLog.error("2. Error closing database in sheets.js " + e);
+            }
+        }
+        return result;
     },
 });
