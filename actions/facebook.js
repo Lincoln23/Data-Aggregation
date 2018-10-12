7bfb6e70102c44d9a6ed920b4f50a8b034feed61
Lincoln <Lolincoln23@gmail.com>
2018-10-12 14:47:05 -0400

    code cleanup

--- actions/facebook.js
+++ actions/facebook.js
@@ -5,9 +5,8 @@ let facebook = require('@datafire/facebook').actions;
 //TODO future integration
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-    let result = await facebook.post.likes.get({
-      post: "498884850566017",
+      return await facebook.post.likes.get({
+        post: "498884850566017",
     }, context);
-    return result;
   },
 });
--- actions/stripe.js
+++ actions/stripe.js
@@ -4,7 +4,6 @@ let stripe = require('@datafire/stripe').actions;
 //TODO in the future
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-    let account = await stripe.v1.account.get({}, context);
-    return account;
+      return await stripe.v1.account.get({}, context);
   },
 });
